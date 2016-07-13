4c89ae773eaf5773b2f4b833ad247ec29d554367
Dmitry Barashev <dbarashev@bardsoftware.com>
2016-07-13 20:46:26 +0300

    use rtd theme

--- conf.py
+++ conf.py
@@ -29,7 +29,11 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-RTD_NEW_THEME = True
+import sphinx_rtd_theme
+
+html_theme = "sphinx_rtd_theme"
+
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
